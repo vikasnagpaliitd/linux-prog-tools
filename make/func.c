f74314c9a0b423ecd62d1f07e7529b60c191752b
Vikas Nagpal <vikas.nagpal@gmail.com>
2022-03-11 11:24:39 +0530

    .

---
+++ make/func.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+void func1()
+{
+   printf("func1 is called\n");
+}
---
+++ make/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "func.h"
+
+int main()
+{
+  printf("hello world\n");
+
+  func1();
+
+  return 0;
+}
